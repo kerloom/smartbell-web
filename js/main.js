5ba17db245cfcabe07d69636ec4f5843c1a81161
Chich Alanis <cesar@homeclick.com.mx>
2017-09-13 19:35:08 -0500

    Actualización segunda fase

--- js/main.js
+++ js/main.js
@@ -1,52 +1,4 @@
-/**
- *
- * Bootstrap one-page template with Parallax effect | Script Tutorials
- * http://www.script-tutorials.com/bootstrap-one-page-template-with-parallax-effect/
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2014, Script Tutorials
- * http://www.script-tutorials.com/
- */
-
-$(document).ready(function (){
-
-  // create a LatLng object containing the coordinate for the center of the map
-  var latlng = new google.maps.LatLng(20.589325,-100.395437);
-
-  // prepare the map properties
-  var options = {
-    zoom: 15,
-    center: latlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    navigationControl: true,
-    mapTypeControl: false,
-    scrollwheel: false,
-    disableDoubleClickZoom: true
-  };
-
-  // initialize the map object
-  var map = new google.maps.Map(document.getElementById('google_map'), options);
-
-  // add Marker
-  var marker1 = new google.maps.Marker({
-    position: latlng, map: map
-  });
-
-  // add listener for a click on the pin
-  google.maps.event.addListener(marker1, 'click', function() {
-    infowindow.open(map, marker1);
-  });
-
-  // add information window
-  var infowindow = new google.maps.InfoWindow({
-    content:  '<div class="info"><strong>This is my company</strong><br><br>My company address is here<br> 32846 Sydney</div>'
-  });
-
-  //Skrollr for parallax
-  var s = skrollr.init();
-});
+ 
 
 $(function() {  
     var glower = $('#glower');
@@ -54,3 +6,15 @@ $(function() {
         glower.toggleClass('active');
     }, 1000);
 });
+
+function initMap() {
+        var uluru = {lat: 20.5893200, lng: -100.3932430};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 15,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
