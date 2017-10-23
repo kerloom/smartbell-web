497bf907dcf0417b3548011d28a7a888e7712a6a
Chich Alanis <cesar@homeclick.com.mx>
2017-10-23 18:24:09 -0500

    primer prueba cambio de resolucion de imagen index

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $(function() {
 function initMap() {
         var uluru = {lat: 20.5893200, lng: -100.3932430};
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 15,
+          zoom: 14,
           center: uluru
         });
         var marker = new google.maps.Marker({
